c65bf3385069ba76a39969763f10e2ac897d2fcb
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2025-03-27 15:06:28 +0300

    Initial commit

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+
+{
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        logic = GameObject.FindWithTag("logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive == true)
+
+        {
+            myRigidbody.linearVelocity = Vector2.up * flapStrength;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
